42ad698bdce833bd6051e00863ace90f5df06889
mebusw <mebusw@163.com>
2012-06-26 23:07:23 +0800

    add dependency: webapp

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='mebusw',
+      author_email='mebusw@163.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
+      install_requires=['web.py>=0.36'],
      )
