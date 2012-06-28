ad0eda89088f272c8e1b434061a319d397a47742
mebusw <mebusw@163.com>
2012-06-28 22:59:01 +0800

    enable flask debugger

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -3,9 +3,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/debug")
+def debug():
+    raise
+    return "Debugging Flask..."
+
 @app.route("/")
 def hello():
     return "Hello Flask!"
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
