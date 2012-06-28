a1852caad441835e57c295684a884365eae4af20
mebusw <mebusw@163.com>
2012-06-28 22:54:34 +0800

    correct dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(name='OpenShift Mongo Twt',
       author_email='matwood@redhat.com',
       url='https://github.com/openshift/openshift-twt-mongo-demo',
       # dont install bottle requirement, bottle is included in source
-      install_requires=['pymongo'],
-      install_requires=['Flask>=0.7.2']
+      install_requires=['pymongo', 'Flask>=0.7.2']
      )
