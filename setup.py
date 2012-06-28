b428576a87fb266e43c392dd52cf03e203d60497
mebusw <mebusw@163.com>
2012-06-28 22:53:15 +0800

    author name

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='OpenShift Mongo Twt',
       version='1.0',
       description='OpenShift Twitter clone using MongoDB',
-      author='Mark Atwood',
+      author='mebusw',
       author_email='matwood@redhat.com',
       url='https://github.com/openshift/openshift-twt-mongo-demo',
       # dont install bottle requirement, bottle is included in source
       install_requires=['pymongo'],
-      install_requires=['Flask>=0.7.2'],
+      install_requires=['Flask>=0.7.2']
      )
