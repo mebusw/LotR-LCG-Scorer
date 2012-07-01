05d79a082c7eae5f0fed6fac0d7da4c01861415f
mebusw <mebusw@163.com>
2012-07-01 14:06:27 +0800

    route handlers

--- wsgi/chanlloge.py
+++ wsgi/chanlloge.py
@@ -4,19 +4,12 @@
 import os
 import uuid
 import bottle
-import pymongo
+#import pymongo
 import time
 import simplejson as json
 
 bottle.debug(True)
 
-mongo_con = pymongo.Connection(
-  os.environ['OPENSHIFT_NOSQL_DB_HOST'],
-  int(os.environ['OPENSHIFT_NOSQL_DB_PORT']))
-
-mongo_db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
-mongo_db.authenticate(os.environ['OPENSHIFT_NOSQL_DB_USERNAME'],
-                      os.environ['OPENSHIFT_NOSQL_DB_PASSWORD'])
 
 
 #################################
@@ -27,15 +20,39 @@ mongo_db.authenticate(os.environ['OPENSHIFT_NOSQL_DB_USERNAME'],
 
 @bottle.route('/tournaments/', method='GET')
 def tournaments_index():
-    return json.dumps([u'index', {u'bar': [u'baz', None, 1.0, 2]}])
+    return json.dumps(['tournaments', 'index', [u'baz', None, 1.0, 2]])
 
-@bottle.route('/tournaments/<tid>', method='GET')
+@bottle.route('/tournaments/<tid:int>', method='GET')
 def tournaments_show(tid):
-    return json.dumps([u'show', {u'tid': [u'baz', None, 1.0, 2]}])
+    return json.dumps(['tournaments', 'show', {'tid': tid}, [u'baz', None, 1.0, 2]])
+
+@bottle.route('/tournaments/<tid>', method='POST')
+def tournaments_publish(tid):
+    return json.dumps(['tournaments', 'publish', {'tid': tid}, [u'baz', None, 1.0, 2]])
 
 
 
+@bottle.route('/tournaments/<tid>/participants', method='GET')
+def participants_index(tid):
+    return json.dumps(['participants', 'index', {'tid': tid}, [u'baz', None, 1.0, 2]])
 
+@bottle.route('/tournaments/<tid>/participants/<pid>', method='PUT')
+def participants_update(tid, pid):
+    return json.dumps(['participants', 'update', {'tid': tid}, {'pid': pid}, [u'baz', None, 1.0, 2]])
+
+
+#@bottle.route('/static/<filename>')
+#def server_static(filename):
+#    return bottle.static_file(filename, root='/Users/mebusw/Desktop/lotr-cloud/lotr/wsgi/static')
+
+@bottle.route('/scorer/<path:path>')
+def server_static(path):
+    return bottle.static_file(path, root='/Users/mebusw/Desktop/lotr-cloud/lotr/wsgi/static/lotr-scorer')
+
+
+if __name__ == '__main__':
+    bottle.default_app()
+    bottle.run(reloader=True)
 
 
 
