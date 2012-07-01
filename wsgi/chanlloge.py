d1afe616d6a6d209b18c83de9e870c77936e2d41
mebusw <mebusw@163.com>
2012-07-01 12:41:24 +0800

    back to simplejson rather jsonplugin of bottle

--- wsgi/chanlloge.py
+++ wsgi/chanlloge.py
@@ -6,7 +6,7 @@ import uuid
 import bottle
 import pymongo
 import time
-#import simplejson as json
+import simplejson as json
 
 bottle.debug(True)
 
@@ -27,11 +27,11 @@ mongo_db.authenticate(os.environ['OPENSHIFT_NOSQL_DB_USERNAME'],
 
 @bottle.route('/tournaments/', method='GET')
 def tournaments_index():
-    return [u'index', {u'bar': [u'baz', None, 1.0, 2]}]
+    return json.dumps([u'index', {u'bar': [u'baz', None, 1.0, 2]}])
 
 @bottle.route('/tournaments/<tid>', method='GET')
 def tournaments_show(tid):
-    return [u'show', {u'tid': [u'baz', None, 1.0, 2]}]
+    return json.dumps([u'show', {u'tid': [u'baz', None, 1.0, 2]}])
 
 
 
